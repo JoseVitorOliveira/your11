b61d8a0c95975f1650d9d2292df65781004ef39f
José Vítor <jose.vitor.oliveira.44@gmail.com>
2024-03-12 20:59:03 -0300

    tactics selector component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import FootballField from "./components/FootballField";
 
 function App() {
+  const [formation, setFormation] = useState("4-3-3");
+
+  const handleFormationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormation(e.target.value);
+  };
+
   return (
     <>
-      <FootballField></FootballField>
+      <div>
+        <label htmlFor="formation">Select Formation:</label>
+        <select
+          id="formation"
+          value={formation}
+          onChange={handleFormationChange}
+        >
+          <option value="4-4-2">4-4-2</option>
+          <option value="4-3-3">4-3-3</option>
+        </select>
+      </div>
+      <FootballField formation={formation} />
     </>
   );
 }
--- src/components/FootballField/index.tsx
+++ src/components/FootballField/index.tsx
@@ -1,86 +1,56 @@
 import Player from "../Player";
 import "./styles.css";
 
-const FootballField = () => {
+interface FootballFieldProps {
+  formation: string;
+}
+
+const FootballField = (formation: FootballFieldProps) => {
+  const formations = {
+    "4-3-3": [
+      { leftPos: 160, topPos: 370 },
+      { leftPos: 200, topPos: 310 },
+      { leftPos: 120, topPos: 310 },
+      { leftPos: 280, topPos: 290 },
+      { leftPos: 40, topPos: 290 },
+      { leftPos: 120, topPos: 200 },
+      { leftPos: 200, topPos: 200 },
+      { leftPos: 40, topPos: 70 },
+      { leftPos: 160, topPos: 50 },
+      { leftPos: 160, topPos: 140 },
+      { leftPos: 280, topPos: 70 },
+    ],
+    "4-4-2": [
+      { leftPos: 160, topPos: 370 },
+      { leftPos: 200, topPos: 310 },
+      { leftPos: 120, topPos: 310 },
+      { leftPos: 280, topPos: 290 },
+      { leftPos: 40, topPos: 290 },
+      { leftPos: 120, topPos: 200 },
+      { leftPos: 200, topPos: 200 },
+      { leftPos: 280, topPos: 200 },
+      { leftPos: 40, topPos: 70 },
+      { leftPos: 160, topPos: 70 },
+    ],
+  };
+
+  const playerPositions =
+    formations[formation.formation as keyof typeof formations] || [];
+
   return (
     <div className="football-field">
-      <Player
-        leftPos={160}
-        topPos={370}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 1"
-      />
-      <Player
-        leftPos={200}
-        topPos={310}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 2"
-      />
-      <Player
-        leftPos={120}
-        topPos={310}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 3"
-      />
-      <Player
-        leftPos={280}
-        topPos={290}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 4"
-      />
-      <Player
-        leftPos={40}
-        topPos={290}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 5"
-      />
-      <Player
-        leftPos={120}
-        topPos={200}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 6"
-      />
-      <Player
-        leftPos={200}
-        topPos={200}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 7"
-      />
-      <Player
-        leftPos={40}
-        topPos={70}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 8"
-      />
-      <Player
-        leftPos={160}
-        topPos={50}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 9"
-      />
-      <Player
-        leftPos={160}
-        topPos={140}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 10"
-      />
-      <Player
-        leftPos={280}
-        topPos={70}
-        primaryColor="red"
-        secondaryColor="white"
-        name="Player 11"
-      />
+      {playerPositions.map(
+        (position: { topPos: number; leftPos: number }, index: number) => (
+          <Player
+            key={index}
+            topPos={position.topPos}
+            leftPos={position.leftPos}
+            primaryColor="red"
+            secondaryColor="white"
+            name={`Player ${index + 1}`}
+          />
+        )
+      )}
     </div>
   );
 };
