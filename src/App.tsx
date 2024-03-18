a839e85da69e84903bd0d545b98996b68b8eff55
José Vítor <jose.vitor.oliveira.44@gmail.com>
2024-03-18 18:11:55 -0300

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -31,49 +31,53 @@ function App() {
     <>
       <Header />
       <main>
-        <div className="formation-container">
-          <label htmlFor="formation">Select Formation:</label>
-          <select
-            id="formation"
-            value={formation}
-            onChange={handleFormationChange}
+        <div className="options-container">
+          <div className="formation-container">
+            <label htmlFor="formation">Select Formation:</label>
+            <select
+              id="formation"
+              value={formation}
+              onChange={handleFormationChange}
+            >
+              <option value="4-3-3">4-3-3</option>
+              <option value="4-3-2-1">4-3-2-1</option>
+              <option value="4-4-2">4-4-2</option>
+              <option value="4-4-1-1">4-4-1-1</option>
+              <option value="4-5-1">4-5-1</option>
+              <option value="3-5-2">3-5-2</option>
+              <option value="3-4-3">3-4-3</option>
+            </select>
+          </div>
+          <div className="colors-container">
+            <ColorPicker
+              color={primaryColor}
+              onChange={handlePrimaryColorChange}
+              label="Primary Color"
+            />
+            <ColorPicker
+              color={secondaryColor}
+              onChange={handleSecondaryColorChange}
+              label="Secondary Color"
+            />
+          </div>
+        </div>
+        <div className="field-save-container">
+          <div ref={exportRef}>
+            <FootballField
+              primaryColor={primaryColor}
+              secondaryColor={secondaryColor}
+              formation={formation}
+            />
+          </div>
+          <button
+            className="save-button"
+            onClick={() =>
+              exportRef.current && exportAsImage(exportRef.current, "formation")
+            }
           >
-            <option value="4-3-3">4-3-3</option>
-            <option value="4-3-2-1">4-3-2-1</option>
-            <option value="4-4-2">4-4-2</option>
-            <option value="4-4-1-1">4-4-1-1</option>
-            <option value="4-5-1">4-5-1</option>
-            <option value="3-5-2">3-5-2</option>
-            <option value="3-4-3">3-4-3</option>
-          </select>
+            Save as PNG <img src={downloadIcon} alt="download icon" />
+          </button>
         </div>
-        <div className="colors-container">
-          <ColorPicker
-            color={primaryColor}
-            onChange={handlePrimaryColorChange}
-            label="Primary Color"
-          />
-          <ColorPicker
-            color={secondaryColor}
-            onChange={handleSecondaryColorChange}
-            label="Secondary Color"
-          />
-        </div>
-        <div ref={exportRef}>
-          <FootballField
-            primaryColor={primaryColor}
-            secondaryColor={secondaryColor}
-            formation={formation}
-          />
-        </div>
-        <button
-          className="save-button"
-          onClick={() =>
-            exportRef.current && exportAsImage(exportRef.current, "formation")
-          }
-        >
-          Save as PNG <img src={downloadIcon} alt="download icon" />
-        </button>
       </main>
       <Footer />
     </>
